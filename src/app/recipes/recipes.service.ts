8e6298d238321bb916f9b0aaba6ebd08d3b5a27e
Adrian Puscasu <adipuscasu@users.noreply.github.com>
2020-04-25 20:10:44 +0300

    adds the deleteRecipe

--- src/app/recipes/recipes.service.ts
+++ src/app/recipes/recipes.service.ts
@@ -32,4 +32,10 @@ export class RecipesService {
       })
     };
   }
+
+  public deleteRecipe(recipeId: string) {
+    this.recipes = this.recipes.filter(recipe => {
+      return recipe.id !== recipeId;
+    });
+  }
 }
